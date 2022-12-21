4dfacfdcfd7c53b65a1dcc5bac0397a76d33725d
Danil26m <111749790+Danil26m@users.noreply.github.com>
2022-12-21 18:54:23 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,7 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-
-export class Button extends Component {
-  render() {
-    return (
-      <button className='Button' type='button' onClick={this.props.click}>Load more</button>
-    )
-  }
+export default function Button({click}) {
+  return <button className='Button' type='button' onClick={click}>Load more</button>  
 }
 Button.propTypes={
   click:PropTypes.func
 }
-export default Button
