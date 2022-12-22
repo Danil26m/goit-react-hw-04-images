4327d1606b8d263bd53779ca917d3828220ab643
Danil26m <111749790+Danil26m@users.noreply.github.com>
2022-12-22 19:04:06 +0200

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,33 +1,34 @@
-import PropTypes from 'prop-types'
-import { useEffect } from 'react'
-export default function Modal({bigImage,togle}) {
-const  close=(e)=>{
-  if(e.key === 'Escape'){
+import PropTypes from 'prop-types';
+import { useEffect } from 'react';
+export default function Modal({ bigImage, togle }) {
+  const close = e => {
+    if (e.key === 'Escape') {
       togle();
-  }
+    }
+  };
+  const clickClose = e => {
+    const r = document.querySelector('.Overlay');
+    if (r === e.srcElement) {
+      togle();
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('keydown', close);
+    document.querySelector('.Overlay').addEventListener('click', clickClose);
+    return () => {
+      window.removeEventListener('keydown', close);
+      window.removeEventListener('click', clickClose);
+    };
+  });
+  return (
+    <div className="Overlay">
+      <div className="Modal">
+        <img src={bigImage} alt="" />
+      </div>
+    </div>
+  );
 }
- const  clickClose=(e)=>{
-             const r = document.querySelector('.Overlay')
-             if(r === e.srcElement){
-              togle();
-             }
-          }
-    useEffect(()=>{
-        window.addEventListener('keydown',close);
-        document.querySelector('.Overlay').addEventListener('click',clickClose);
-        return ()=>{
-          window.removeEventListener('keydown',close);
-        }
-    })
-    return <div className="Overlay">
-              <div className="Modal">
-                <img src={bigImage} alt="" />
-              </div>
-            </div>
-              
-     
-}
-Modal.propTepes={
-    bigImage: PropTypes.string.isRequired,
-    togle: PropTypes.func
-}
+Modal.propTepes = {
+  bigImage: PropTypes.string.isRequired,
+  togle: PropTypes.func,
+};
