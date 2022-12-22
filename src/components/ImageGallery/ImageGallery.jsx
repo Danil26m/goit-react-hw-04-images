585795537aa167fd87ccc2803fdf4104f080398e
Danil26m <111749790+Danil26m@users.noreply.github.com>
2022-12-22 13:47:11 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,14 +3,14 @@ import { Audio } from 'react-loader-spinner'
 import Modal from '../Modal'
 import Notiflix from 'notiflix';
 import Searchbar from '../Searchbar/Searchbar';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useRef, useEffect  } from 'react';
 export default function ImageGallery() {
 const [img, setImg] = useState([]);
 const [page, setPage] = useState(1);
 const [loading, setLoading] = useState(false);
 const [isModal, setIsModal] = useState('');
 const [filter, setFilter] = useState('');
+const isFirstRender = useRef(true)
 const handelClick=()=>{
   setPage(state=> state + 1)
           
@@ -24,9 +24,12 @@ const handelClick=()=>{
                   setFilter(newFilter);
                   setImg([]);
                   setPage(1);
-                  // this.setState({ filter: newFilter,img:[],page:1});
                 }
                 useEffect(()=>{
+                  if(isFirstRender.current){
+                    isFirstRender.current = false;
+                    return
+                  }
                   setLoading(true)
                             const baseUrl = `https://pixabay.com/api/?q=${filter}&page=${page}&key=27491785-4ed714c8d697aeb5a6480b366&image_type=photo&orientation=horizontal&per_page=12`;
                               fetch(baseUrl).then(response =>{ 
@@ -47,7 +50,6 @@ const handelClick=()=>{
                 },[filter , page])
              const   togleModal=(src)=>{
                   setIsModal(src?src:'')
-                          this.setState({isModal: src?src:''})
                         }
                 return <main>
                                 <Searchbar submit={handelForm}/>
@@ -84,88 +86,3 @@ const handelClick=()=>{
                       
 }
 
-// export class ImageGallery extends Component {
-//     state ={
-//         img: [],
-//         page: 1,
-//         loading: false,
-//         isModal: '',
-//         filter: '',
-//     }
-//     handelClick=()=>{
-//         this.setState((prev)=>({page: prev.page + 1}))
-//       }
-//       handelForm=(event)=>{
-//         event.preventDefault();
-//         const newFilter = event.target.lastChild.value;
-//         if (newFilter === this.state.filter) {
-//           return
-//         }
-//         this.setState({ filter: newFilter,img:[],page:1});
-//       }
-//     componentDidUpdate(prevProps, prevState){
-//       const firstName = prevState.filter;
-//       const secondName = this.state.filter;
-//       const page = this.state.page;
-//       const pageProp = prevState.page;
-//       console.log(this.state);
-//       if (firstName !== secondName || page !== pageProp) {
-//           this.setState({loading: true});
-//           const baseUrl = `https://pixabay.com/api/?q=${secondName}&page=${page}&key=27491785-4ed714c8d697aeb5a6480b366&image_type=photo&orientation=horizontal&per_page=12`;
-//             fetch(baseUrl).then(response =>{ 
-//               if (!response.ok) {
-//               throw new Error(response.status);
-//             }
-//                 return response.json();
-//         }).then(({hits})=>{  
-       
-//           if (!hits.length) {
-//            return Notiflix.Notify.failure("No result");
-//           }
-//           return  this.setState(prev=>({img:[...prev.img, ...hits]}));
-       
-//     }).catch((err)=>console.log(err))
-//     .finally(()=> this.setState({loading: false}))
-//         }
-//       }
-//       togleModal=(src)=>{
-//         this.setState({isModal: src?src:''})
-//       }
-//   render() {
-//     const{img,isModal,loading} = this.state;
-//     return (
-//         <main>
-//               <Searchbar submit={this.handelForm}/>
-//             {loading && 
-//             <div style={{position: 'absolute',
-//              top:'50%',
-//              left:'50%',
-//               transform: 'translate(-50%,-50%)'
-//               }}>
-//               <Audio
-//                 height="80"
-//                 width="80"
-//                 radius="9"
-//                 color="green"
-//                 ariaLabel="loading"
-//                 wrapperStyle
-//                 wrapperClass
-//                 />  
-//             </div>
-                
-//             }
-//              <ul className="ImageGallery">
-//             {img.length?img.map((m,i)=>(
-//                 <li className='ImageGalleryItem' key={m.id}>
-//                     <img className='ImageGalleryItem-image' src={m.webformatURL} alt={m.tags} onClick={()=>this.togleModal(m.largeImageURL)}/>
-//                 </li>
-//             )):""}
-//       </ul>
-//       {img.length?<Button click={this.handelClick} />:''}
-//       {isModal && <Modal bigImage={isModal} togle={this.togleModal}/>}
-//         </main>
-       
-//     )
-//   }
-// }
-// export default ImageGallery;
